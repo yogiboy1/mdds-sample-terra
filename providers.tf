579e802a1bba12cd988684964e299ec67d5b22b9
yogiboy1 <88436258+yogiboy1@users.noreply.github.com>
2023-04-17 16:07:01 +0530

    Update and rename provider.tf to providers.tf

--- providers.tf
+++ providers.tf
@@ -2,15 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>4.55.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~>2.3.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~>4.0.0"
     }
   }
 }
