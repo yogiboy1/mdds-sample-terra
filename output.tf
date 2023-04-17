ba3808733521156a0a396c798c62db10ab0d8b52
yogiboy1 <88436258+yogiboy1@users.noreply.github.com>
2023-04-17 18:23:35 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,12 @@
-#output "ec2_remote_access" {
- # description = "EC2 Remote Access"
-  #value       = "ssh -i ${local_file.private_key_pem.filename} ec2-user@${aws_instance.mdds_server.public_ip}"
-#}
+output "ec2_remote_access" {
+  description = "EC2 Remote Access"
+  value       = "ssh -i ${local_file.private_key_pem.filename} ec2-user@${aws_instance.mdds_server.public_ip}"
+}
 
-#output "instance_public_ip" {
- # description = "Public IP address of the Jenkins EC2 instance"
-  #value       = "Jenkins Server Public IP: ${aws_instance.mdds_server.public_ip}"
-#}
+output "instance_public_ip" {
+  description = "Public IP address of the Jenkins EC2 instance"
+  value       = "Jenkins Server Public IP: ${aws_instance.mdds_server.public_ip}"
+}
 
 output "s3_bucket_uri" {
   description = "S3 bucket URI"
