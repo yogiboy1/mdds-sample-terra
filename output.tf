c8a3f4feff162041890b2bf82d39d2085992cb32
yogiboy1 <88436258+yogiboy1@users.noreply.github.com>
2023-04-17 15:37:04 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,19 @@
-
-output "public_ip" {
- value       = aws_instance.my_vm.public_ip
- description = "Public IP Address of EC2 instance"
+output "ec2_remote_access" {
+  description = "EC2 Remote Access"
+  value       = "ssh -i ${local_file.private_key_pem.filename} ec2-user@${aws_instance.mdds_server.public_ip}"
 }
 
-output "instance_id" {
- value       = aws_instance.my_vm.id
- description = "Instance ID"
+output "instance_public_ip" {
+  description = "Public IP address of the Jenkins EC2 instance"
+  value       = "Jenkins Server Public IP: ${aws_instance.mdds_server.public_ip}"
+}
+
+output "s3_bucket_uri" {
+  description = "S3 bucket URI"
+  value       = "S3 Bucket URI: s3://${aws_s3_bucket.s3.id}"
+}
+
+output "s3_bucket_url" {
+  description = "S3 Bucket URL"
+  value       = "S3 Bucket URL: https://${aws_s3_bucket.s3.bucket_domain_name}"
 }
