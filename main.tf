418a6d061f5b125218876fc681bdc0476571a1c6
yogiboy1 <88436258+yogiboy1@users.noreply.github.com>
2023-04-17 16:52:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,9 +33,9 @@ module "mdds_vpc" {
   name = "mdds-vpc"
   cidr = var.vpc_cidr
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["${var.aws_region}a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = true
 
